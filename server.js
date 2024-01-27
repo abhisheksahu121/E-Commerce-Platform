9c8cdb6013e5ce9b9f839a6904ee90fa34927d6b
abhishek sahu <abhishek4032sahu@gmail.com>
2024-01-27 17:30:28 +0530

    MongoDB Connection Stabliesed

---
+++ server.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import connectDB from './config/db.js';
+//configure env
+// env file created on root directory so no need to pass the path object into config
+dotenv.config();
+
+//database
+connectDB();
+//rest obj
+const app = express();
+
+//middelwares
+app.use(express.json());
+app.use(morgan('dev'));
+//rest api
+app.get('/', (req, res) => {
+    res.send("<h1>Welcome to Ecommerce app</h1>")
+})
+
+//PORT
+const PORT = process.env.PORT || 8080;
+
+// run listen
+app.listen(PORT, () => {
+    console.log(`Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`)
+})
